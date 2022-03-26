7afee7a56e08b61356e7dd406f0579d6fc2c8d5f
jksolucoesti <jksolucoesti@gmail.com>
2022-03-25 21:27:27 -0300

    Projeto Inicial

---
+++ Arduino/Arduino/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Arduino
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkConectar = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 86);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(277, 334);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Controle Temperatura";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(12, 60);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(54, 20);
+            this.status.TabIndex = 5;
+            this.status.Text = "Online";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkConectar
+            // 
+            this.checkConectar.AutoSize = true;
+            this.checkConectar.Location = new System.Drawing.Point(12, 40);
+            this.checkConectar.Name = "checkConectar";
+            this.checkConectar.Size = new System.Drawing.Size(69, 17);
+            this.checkConectar.TabIndex = 6;
+            this.checkConectar.Text = "Conectar";
+            this.checkConectar.UseVisualStyleBackColor = true;
+            this.checkConectar.CheckedChanged += new System.EventHandler(this.checkConectar_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(211, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 66);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(211, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(81, 66);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(82, 57);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(123, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "LOG";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 426);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(19, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "ABRIR ARQUIVO DE LOGS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 488);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkConectar);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "COMUNICADOR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.IO.Ports.SerialPort serialPort1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label status;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.CheckBox checkConectar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Arduino/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace Arduino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            //serialPort1.Open();
+            status.ForeColor = Color.Red;
+            ImagemClr();
+            CampoDownload();
+            ObtemDiretorioAplicacao();
+        }
+        public void LerSerial()
+        {
+            if (serialPort1.IsOpen)
+            {
+                if (serialPort1.BytesToRead > 0)
+                {
+                    byte[] rxBuff = new byte[serialPort1.BytesToRead];
+                    int nRxed = serialPort1.Read(rxBuff, 0, serialPort1.BytesToRead);
+                    string act8 = System.Text.Encoding.ASCII.GetString(rxBuff);
+
+                    richTextBox1.Text += $"Temperatura = {act8} Data { DateTime.Now} \n";
+
+                    if (!File.Exists(textBox1.Text + "\\log.txt"))
+                    {
+                       File.CreateText(textBox1.Text + "\\log.txt").Close();
+                    }
+                    else
+                    {
+                        GeradorDeLogs($"Temperatura = {act8} Data { DateTime.Now}");
+                    }
+                }
+            }
+        }
+        private void checkConectar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkConectar.Checked == true)
+            {
+                richTextBox1.Text = "Comunicação ativa \n";
+                richTextBox1.Text += "-------------------------------\n\n";
+                pictureBox2.Visible = false;
+                pictureBox1.Visible = true;
+                progressBar1.Visible = true;
+                status.ForeColor = Color.Green;
+                timer1.Start();
+            }
+            if (checkConectar.Checked == false)
+            {
+                richTextBox1.Text = "Comunicação desativada \n";
+                pictureBox2.Visible = true;
+                pictureBox1.Visible = false;
+                progressBar1.Visible = false;
+                status.ForeColor = Color.Red;
+                timer1.Stop();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LerSerial();
+        }
+        private void ImagemClr()
+        {
+            pictureBox1.Visible = false;
+        }
+
+        private void ImagemPb()
+        {
+            pictureBox2.Visible = true;
+        }
+        private void CampoDownload()
+        {
+            progressBar1.Visible = false;
+        }
+
+        private void ObtemDiretorioAplicacao()
+        {
+            textBox1.Text = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+            textBox1.Enabled = false;
+        }
+
+        private void GeradorDeLogs(string log)
+        {
+            using (StreamWriter writer = new StreamWriter(textBox1.Text + "\\log.txt", true))
+            {
+                writer.WriteLine(log);
+                writer.Close();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string diretorio = textBox1.Text +"\\log.txt";
+            try
+            {
+                System.Diagnostics.Process.Start("notepad", diretorio);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
+
